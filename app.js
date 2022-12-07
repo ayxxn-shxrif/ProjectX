a8bb4da636c461f0ff94c27eb30fb9b93a912c5b
ayxxn-shxrif <ayaansharif42@gmail.com>
2022-12-07 11:48:17 +0100

    Initialize passportjs session

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var signupRouter = require('./routes/sign-up');
+const indexRouter = require('./routes/index');
+const signupRouter = require('./routes/sign-up');
 
 dotenv.config();
 
@@ -16,13 +18,16 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
