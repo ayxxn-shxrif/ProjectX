606ad6a67757110596e7bd1ec81e961dbc5055a8
ayxxn-shxrif <ayaansharif42@gmail.com>
2022-12-07 11:26:09 +0100

    Create message model

---
+++ models/message.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const MessageSchema = new Schema({
+  title: { type: String, required: true, maxLength: 150 },
+  text: { type: String, required: true, maxLength: 500 },
+  timestamp: { type: Date, default: Date.now, required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
