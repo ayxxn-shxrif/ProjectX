4cd6ffc2239d8a57ea72ac19b8e2f61b75c3e44d
ayxxn-shxrif <ayaansharif42@gmail.com>
2022-12-08 15:45:49 +0100

    Create message virtual with a formatted date

--- models/message.js
+++ models/message.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { format } = require('date-fns');
 
 const { Schema } = mongoose;
 
@@ -7,6 +8,10 @@ const MessageSchema = new Schema({
   text: { type: String, required: true, maxLength: 500 },
   timestamp: { type: Date, default: Date.now },
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+MessageSchema.virtual('date_formatted').get(function () {
+  return format(this.timestamp, 'd MMMM yyyy');
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
