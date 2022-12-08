e047f1e1651a1a91afbdca04992f65a36b8e21a3
ayxxn-shxrif <ayaansharif42@gmail.com>
2022-12-08 14:12:59 +0100

    Change club membership status in db when user enters secret password

--- controllers/joinController.js
+++ controllers/joinController.js
@@ -1,5 +1,32 @@
+const { body, validationResult } = require('express-validator');
+const User = require('../models/user');
+
 exports.join_get = (req, res) => {
-  res.render('join');
+  res.render('join', { user: req.user });
 };
 
-exports.join_post = (req, res, next) => {};
+exports.join_post = [
+  body('password')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Enter a password')
+    .bail()
+    .matches('1234')
+    .withMessage('Wrong password!'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('join', {
+        errors: errors.array(),
+        user: req.user,
+      });
+    }
+    User.findByIdAndUpdate(req.user.id, { is_member: true }, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect('/');
+    });
+  },
+];
