b1fb68b4452907dece9935ee4b438146c99d50c7
ayxxn-shxrif <ayaansharif42@gmail.com>
2023-01-02 16:11:53 +0530

    script to install dependencies and run dev-server

---
+++ npm_install_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if npm audit fix is needed
+npm audit
+
+# If npm audit finds vulnerabilities, run npm audit fix
+if [ $? -eq 1 ]; then
+  npm audit fix
+
+  # If npm audit fix fails, run npm audit fix --force
+  if [ $? -eq 1 ]; then
+    npm audit fix --force
+  fi
+fi
+
+# Install dependencies
+npm install && node script.js
